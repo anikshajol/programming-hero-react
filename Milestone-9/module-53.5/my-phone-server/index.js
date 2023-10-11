5e61b57a656b4c2244bf4a0df64a1d6596386614
anikshajol <anikshajol@gmail.com>
2023-10-11 14:50:53 +0600

    milestone 9 bonus video complete

---
+++ Milestone-9/module-53.5/my-phone-server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const phones = require("./phones.json");
+
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world this is phone server is coming soon");
+});
+
+app.get("/phones", (req, res) => {
+  res.send(phones);
+});
+
+app.get("/phones/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log("I need data for id :", id);
+  //   const phon = phones.find((phone) => phone.id === id) || {};
+  const phone = phones.find((phone) => phone.id === id) || {};
+
+  res.send(phone);
+});
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
